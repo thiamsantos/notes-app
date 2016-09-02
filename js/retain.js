989fe8aeeaaa8c68e9ab609e03191c75807af741
thiamsantos <thia.mdossantos@gmail.com>
2016-09-02 11:44:59 -0300

    implementing my solution

--- js/retain.js
+++ js/retain.js
@@ -1,63 +1,96 @@
-$(function(){
+(() => {
 
-    var model = {
-        init: function() {
-            if (!localStorage.notes) {
-                localStorage.notes = JSON.stringify([]);
-            }
-        },
-        add: function(obj) {
-            var data = JSON.parse(localStorage.notes);
-            data.push(obj);
-            localStorage.notes = JSON.stringify(data);
-        },
-        getAllNotes: function() {
-            return JSON.parse(localStorage.notes);
-        }
-    };
+  const $ = document.querySelector.bind(document);
 
+  class Model {
 
-    var octopus = {
-        addNewNote: function(noteStr) {
-            model.add({
-                content: noteStr
-            });
-            view.render();
-        },
+    static init() {
 
-        getNotes: function() {
-            return model.getAllNotes();
-        },
+      if (!localStorage.notes) localStorage.notes = JSON.stringify([]);
 
-        init: function() {
-            model.init();
-            view.init();
-        }
-    };
+    }
+
+    static add(obj) {
+
+      let data = JSON.parse(localStorage.notes);
+      data.push(obj);
+      localStorage.notes = JSON.stringify(data);
+
+    }
+
+    static getAllNotes() {
+
+      return JSON.parse(localStorage.notes);
+
+    }
+
+  }
 
 
-    var view = {
-        init: function() {
-            this.noteList = $('#notes');
-            var newNoteForm = $('#new-note-form');
-            var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
-                octopus.addNewNote(newNoteContent.val());
-                newNoteContent.val('');
-                e.preventDefault();
-            });
-            view.render();
-        },
-        render: function(){
-            var htmlStr = '';
-            octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
-            });
-            this.noteList.html( htmlStr );
-        }
-    };
+  class Octopus {
 
-    octopus.init();
-});
+    static addNewNote(noteStr) {
+
+      Model.add({
+        content: noteStr,
+        date: Date.now()
+      });
+
+      View.render();
+
+    }
+
+    static getNotes() {
+
+      return Model.getAllNotes();
+
+    }
+
+    static init() {
+
+      Model.init();
+      View.init();
+
+    }
+
+  }
+
+  class View {
+
+    static init() {
+
+      this.noteList = $('#notes');
+      const newNoteForm = $('#new-note-form');
+      const newNoteContent = $('#new-note-content');
+      newNoteForm.addEventListener('submit', function(e){
+        Octopus.addNewNote(newNoteContent.value);
+        newNoteContent.value = '';
+        e.preventDefault();
+      });
+
+      View.render();
+
+    }
+
+    static render() {
+
+      let htmlStr = '';
+
+      Octopus.getNotes()
+        .forEach(note => htmlStr +=
+          `<li class="note">
+            ${note.content}
+            <p class="note-date">
+              ${new Date(note.date).toString()}
+            </p>
+          </li>`);
+
+      this.noteList.innerHTML = htmlStr;
+
+    }
+
+  }
+
+  Octopus.init();
+
+})();
